3d0d4fa8081fa6e2fc3bfa95d7f450968fb55a7a
dhanerawalaradiya-ship-it <dhanerawalaradiya@gmail.com>
2026-03-29 00:44:23 +0530

    Update database password in db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = new mysqli("localhost", "root", "", "FitnessTracker");
+$conn = new mysqli("localhost", "root", "rads2007", "FitnessTracker");
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
